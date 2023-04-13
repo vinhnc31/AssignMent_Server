1875250bef52415dee3290bfd887b1284ad5c959
VinhDev <congvinh3110@gmail.com>
2023-04-13 07:38:09 +0700

    assm

--- Controllers/acrout.controller.js
+++ Controllers/acrout.controller.js
@@ -3,6 +3,27 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const validator = require("email-validator");
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 class Signupcontroller {
   indexLogin(req, res) {
     res.render("login", { layout: "index" });
@@ -26,53 +47,62 @@ class Signupcontroller {
         });
         if (validator.validate("test@email.com")) {
           user
-          .save()
-          .then(() => {
-            res.redirect("/");
-          })
-          .catch((err) => {
-            res.status(500).json({ error: err });
-          });
+            .save()
+            .then(() => {
+              res.redirect("/");
+            })
+            .catch((err) => {
+              res.status(500).json({ error: err });
+            });
         } else {
-          console.log("email sai dinh dang")
-        }   
+          console.log("email sai dinh dang");
+        }
       }
     });
   }
- login(req, res, next) {
+  login(req, res) {
     var username = req.body.username;
     var password = req.body.password;
-
     userModel
-      .findOne({email: username, password: password})
-      .then((user) => {
-        if (user) {
-          bcrypt.compare(password, user.password, (err, result) => {
-            if (err) {
-              res.status(550).json({ error: err });
-            }
-            if (result) {
-              let token = jwt.sign(
-                { username: userModel.username },
-                "verySecretValue",
-                { expiresIn: "1h" }
-              );
-              res.json({
-                message: "Login Successfull",
-                token,
-              });
+      .findOne({
+        username: username,
+      })
+
+      .then((data) => {
+        if (data) {
+          console.log(password, data.password);
+          bcrypt.compare(password, data.password).then((user) => {
+            console.log(user);
+            if (user) {
+              var token = jwt.sign({ _id: data._id }, "mk");
+              console.log(token)
+              setCookie('token',data.token,1)
             } else {
-              res.json({
-                message: "password do not matched!",
-              });
+              return res.json("sai password");
             }
           });
         } else {
-          res.json({
-            message: " do not matched!",
-          });
+          return res.json("that bai");
         }
+      })
+      .catch((err) => {
+        res.status(500).json("loi sv");
       });
+  }
+  verify(req, res, next) {
+    try {
+      var token = req.params.token;
+      var kq = jwt.verify(token, "mk");
+      if (kq) {
+        next();
+      }
+    } catch (error) {
+      console.log(error);
+      return res.json("ban can phai login");
     }
   }
+  returnhome(req, res, next) {
+    res.redirect("home");
+  }
+}
 module.exports = new Signupcontroller();
--- Routes/acrout.route.js
+++ Routes/acrout.route.js
@@ -2,23 +2,23 @@ const express = require("express");
 const route = express.Router();
 const userController = require("../Controllers/acrout.controller");
 const multer = require("multer");
+const jwt = require("jsonwebtoken");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "upload");
   },
   filename: function (req, file, cb) {
-    cb(
-      null,
-      Date.now() + "." + file.originalname.split(".")[1]
-    );
+    cb(null, Date.now() + "." + file.originalname.split(".")[1]);
   },
 });
 const upload = multer({
   storage,
 });
+
 route.use(express.json());
 route.get("/", userController.indexLogin);
 route.get("/signup", userController.indexSignup);
-route.post("/signup",upload.single('file'),userController.register);
-route.post("/login",userController.login);
+route.post("/signup", upload.single("file"), userController.register);
+route.post("/login", userController.login);
+route.get("/:token", userController.verify, userController.returnhome);
 module.exports = route;
